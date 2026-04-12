f40c442b776cbf9257c7951ac2871c677a235942
Piyush Tupat <piyusheduc.id2004@gmail.com>
2026-04-12 11:53:46 +0530

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    // Main Function
+    public static void main(String args[])
+    {
+
+        // creating Java string using a new keyword
+        String str = new String("Person");
+
+        System.out.println(str);
+    }
+}
